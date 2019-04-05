3a763e3c1b5518f77ab75d8aea87e8ebf3f76a63
sergeev <v.sergeev.m@gmail.com>
2019-04-05 23:06:28 +0700

    Misc...

--- storage/UserStorage.go
+++ storage/UserStorage.go
@@ -2,7 +2,7 @@ package storage
 
 func UserIsValid(uName, pwd string) bool {
 	// DB simulation
-	_uName, _pwd, _isValid := "mayss", "12345", false
+	_uName, _pwd, _isValid := "m", "1", false
 
 	if uName == _uName && pwd == _pwd {
 		_isValid = true
